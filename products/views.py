d2c5e1a45d2f3d5cfdd0ef72e41f7dbfbd970129
Shane Byrne <shanebyrne0963@gmail.com>
2024-01-25 12:41:11 +0000

    Add product sorting functionality

--- products/views.py
+++ products/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 
-# Create your views here.
 
 def all_products(request):
     """
@@ -12,8 +11,11 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        # Search queries
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -22,6 +24,8 @@ def all_products(request):
 
             q = Q(Q(name__icontains=query) | Q(description__icontains=query))
             products = products.filter(q)
+
+        # Categories
         elif 'category' in request.GET:
             category = request.GET['category']
             if not category:
@@ -30,11 +34,28 @@ def all_products(request):
             categories = category.split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
+        
+        # Sorting
+        elif 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products' : products,
         'search_term' : query,
-        'current_categories': categories
+        'current_categories' : categories,
+        'current_sorting' : current_sorting
     }
     return render(request, 'products/products.html', context)
 
