9b7a1f9d8153af6ecd2ded702a7627540ea7db27
Shane Byrne <shanebyrne0963@gmail.com>
2024-01-29 18:23:44 +0000

    Add toast popups for messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, \
+                             get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -12,6 +15,7 @@ def add_to_bag(request, item_id):
     """
     A view to add an item to the bag
     """
+    product = get_object_or_404(Product, id=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -32,6 +36,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+    messages.success(request, f'Added {product.name} to your bag')
     
     request.session['bag'] = bag
     return redirect(redirect_url)
