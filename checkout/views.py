b6f944ad3cc6eb197f97b25b7fcc7723f0e464f0
Shane Byrne <shanebyrne0963@gmail.com>
2024-01-31 10:25:01 +0000

    Add stripe payment section for checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Oead4LrGvNYuhY8Ex36ERmeNJA8RI6jClXcYGEiBvrx4vJtlHlvXkUma2V9c3y4kCioB1NHYVmsTaKNIMEa7WPH00VtJTP1YG',
+        'client_secret': 'test',
     }
 
     return render(request, template, context)
