40e5e55479cb541cb08ec58df5b3d11992d57b56
Shane Byrne <shanebyrne0963@gmail.com>
2024-02-04 19:17:51 +0000

    Add admin delete product view

--- products/views.py
+++ products/views.py
@@ -84,9 +84,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -103,7 +103,7 @@ def edit_product(request, product_id):
     """
     Edit a product
     """
-    product = get_object_or_404(Product, id=product_id)
+    product = get_object_or_404(Product, pk=product_id)
     form = None
 
     if request.method == 'POST':
@@ -124,3 +124,13 @@ def edit_product(request, product_id):
         'product': product,
     }
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Deletes a product from the database
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Successfully deleted product!')
+    return redirect(reverse('products'))
