90df00d3b757cd3f08ac9148ba812a0a506425b1
Shane Byrne <shanebyrne0963@gmail.com>
2024-02-06 11:48:21 +0000

    Fix missing } in STATIC_URL template literal

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -197,7 +197,7 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Override URLs for static and media files
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
